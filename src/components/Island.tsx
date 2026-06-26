9a5bfa1dd419dd7567ec87f5e200235798c0f523
minthantkyaw28 <wanderhobby@gmail.com>
2026-06-26 15:35:06 +0000

    align naming

--- src/components/Island.tsx
+++ src/components/Island.tsx
@@ -417,3 +417,4 @@ const styles = StyleSheet.create({
   dotWrap: { width: 16, alignItems: 'center', justifyContent: 'center' },
   dot: { width: 12, height: 12, borderRadius: 6 },
 });
+// chore: note 2026-06-26T15:35:06
