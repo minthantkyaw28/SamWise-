e94582b31fd0c64f7b95668465a72476c8cc5db6
minthantkyaw28 <wanderhobby@gmail.com>
2026-09-17 22:24:43 +0000

    update sample data

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -89,4 +89,3 @@ const styles = StyleSheet.create({
   hintTitle: { color: colors.islandInk, fontSize: font.bodyLarge, fontWeight: font.weightBold },
   hintText: { color: colors.islandMuted, fontSize: font.body, lineHeight: font.body * 1.4 },
 });
-// chore: note 2026-09-16T22:18:02
